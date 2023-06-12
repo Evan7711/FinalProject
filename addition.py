39947389b0ff242b69e864963408dc12df93dacb
Evan7711 <124279899+Evan7711@users.noreply.github.com>
2023-06-12 03:35:28 +0000

    added missing break statement

--- addition.py
+++ addition.py
@@ -10,5 +10,6 @@ def Addition():
             x = list(map(int, input("Enter all the values you'd like to add: ").split()))
             total = sum(x)
             print(f"The sum is {total}")
+            break
         except ValueError:
             print("Please input numbers only!")
